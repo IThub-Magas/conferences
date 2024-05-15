c4f1ad5cabf990b9fdd652d2380b77ea9869027c
magasoov <77aushev@mail.ru>
2024-05-15 17:25:27 +0300

    123

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,49 +1,49 @@
-$(function() {
-	var marquee = $("#marquee"); 
-	marquee.css({"overflow": "hidden", "width": "100%"});
+$(function () {
+	var marquee = $("#marquee");
+	marquee.css({ "overflow": "hidden", "width": "100%" });
 	// оболочка для текста ввиде span (IE не любит дивы с inline-block)
 	marquee.wrapInner("<span>");
-	marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" }); 
+	marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align": "center" });
 	marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
 	marquee.wrapInner("<div>");
 	marquee.find("div").css("width", "200%");
-	var reset = function() {
+	var reset = function () {
 		$(this).css("margin-left", "0%");
-		$(this).animate({ "margin-left": "-100%" }, 60000, 'linear', reset);
+		$(this).animate({ "margin-left": "-100%" }, 120000, 'linear', reset);
 	};
 	reset.call(marquee.find("div"));
 });
 
-$(function() {
-    var marquee = $("#marquee1");
-    var contentWidth = marquee.find("div").width();
-    
-    marquee.wrapInner("<div class='marquee-inner'>");
-    marquee.find(".marquee-inner").css({
-        "width": "200%", 
-        "margin-left": "-100%" 
-    });
-    
-    var reset = function() {
-        $(this).css("margin-left", "-100%");
-        $(this).animate({ "margin-left": "0%" }, 60000, 'linear', reset);
-    };
-    
-    reset.call(marquee.find(".marquee-inner"));
+$(function () {
+	var marquee = $("#marquee1");
+	var contentWidth = marquee.find("div").width();
+
+	marquee.wrapInner("<div class='marquee-inner'>");
+	marquee.find(".marquee-inner").css({
+		"width": "200%",
+		"margin-left": "-100%"
+	});
+
+	var reset = function () {
+		$(this).css("margin-left", "-100%");
+		$(this).animate({ "margin-left": "0%" }, 40000, 'linear', reset);
+	};
+
+	reset.call(marquee.find(".marquee-inner"));
 });
 
-$(function() {
-	var marquee = $("#marquee2"); 
-	marquee.css({"overflow": "hidden", "width": "100%"});
+$(function () {
+	var marquee = $("#marquee2");
+	marquee.css({ "overflow": "hidden", "width": "100%" });
 	// оболочка для текста ввиде span (IE не любит дивы с inline-block)
 	marquee.wrapInner("<span>");
-	marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" }); 
+	marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align": "center" });
 	marquee.append(marquee.find("span").clone()); // тут у нас два span с текстом
 	marquee.wrapInner("<div>");
 	marquee.find("div").css("width", "200%");
-	var reset = function() {
+	var reset = function () {
 		$(this).css("margin-left", "0%");
-		$(this).animate({ "margin-left": "-100%" }, 60000, 'linear', reset);
+		$(this).animate({ "margin-left": "-100%" }, 120000, 'linear', reset);
 	};
 	reset.call(marquee.find("div"));
 });
